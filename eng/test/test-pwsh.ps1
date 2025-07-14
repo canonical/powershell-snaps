2c50a49c6aaa7cadb2dd1e25cfd7a86bb8a94ac0
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2025-07-14 14:39:46 -0400

    fix: add arch-specific config to test-pwsh script

--- eng/test/test-pwsh.ps1
+++ eng/test/test-pwsh.ps1
@@ -1,13 +1,23 @@
 # Set error action preference to stop on errors
 $ErrorActionPreference = 'Stop'
 try {
-    cd pwsh-repo
+    Set-Location pwsh-repo
 
     Import-Module ./build.psm1
     Restore-PSOptions -PSOptionsPath /snap/powershell*/current/opt/powershell/psoptions.json
 
     $options = (Get-PSOptions)
-    $options.Output = (find /snap/powershell*/current/opt/powershell -type f -name pwsh)
+    $architecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
+
+    if ($architecture -eq 'S390x') {
+        $options.Runtime = 'ubuntu.24.04-s390x'
+        $options.Output = (find /snap/powershell*/current/opt/powershell -type f -name pwsh.dll)
+    } elseif ($architecture -eq 'Ppc64le') {
+        $options.Runtime = 'ubuntu.24.04-ppc64le'
+        $options.Output = (find /snap/powershell*/current/opt/powershell -type f -name pwsh.dll)
+    } else {
+        $options.Output = (find /snap/powershell*/current/opt/powershell -type f -name pwsh)
+    }
 
     Set-PSOptions $options
     Start-PSPester
